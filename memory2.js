29584869b29d47af142781d5812a95072e13498f
maxnawa31 <mnawa31@gmail.com>
2018-01-29 21:51:26 -0800

    adding jquery version

--- memory2.js
+++ memory2.js
@@ -8,66 +8,71 @@ var vanGough = "http://www.theartstory.org/images20/works/van_gogh_vincent_3.jpg
 var guernica = "https://news.artnet.com/app/news-upload/2017/01/guernica_1-256x256.jpg"
 var athens = "https://imgc.artprintimages.com/img/print/print/raphael-school-of-athens-circa-1510-1512-one-of-the-murals-raphael-painted-for-pope-julius-ii_a-l-2572141-8880731.jpg?w=550&h=550"
 var cards = [lasMeninas, lasMeninas, garden, garden, nighthawks, nighthawks, rothko, rothko, oath, oath, guernica, guernica];
-var divs = document.getElementsByClassName("cell")
+var $divs = $(".cell")
 var moma = "http://educationaltravelconsultants.com/blog/wp-content/uploads/moma1.jpg"
 var counter = 0;
 
+// function newGame() {
+//   $divs.each(function() {
+//     $(this).off("click", func) // remove event listeners from each item;
+//   })
+// }
 
 
 //function startGame is called when user presses button;
 function startGame() {
 
-  for (var i = 0; i < divs.length; i++) {
-    divs[i].removeEventListener("click", func) // remove event listeners from each item;
-    divs[i].style.background = 'url(' + moma + ')'; //set background back to original image;
-  }
+  $divs.each(function() {
+    $(this).off("click", func)
+    $(this).css("background-image", 'url(' + moma + ')'); //set background back to original image;
+  })
   counter = 0; //reset counter for score
   var shuffledCards = shuffle(cards); // implements shuffle method
-  document.getElementById('counter').innerHTML = counter;
-  for (var i = 0; i < divs.length; i++) { //itereate over all divs with class of "cell" and attach unique
-    divs[i].dataset.icon = shuffledCards[i]; //image url to its dataset and add class "unmatched";
-    divs[i].classList.add("unmatched");
-    divs[i].style.backgroundPosition = "center"; // style background position
-    divs[i].addEventListener("click", func) //add event listener to each image;
-  }
+  $('#counter').html(counter);
+  $divs.each(function(index) {
+    $(this).attr("data-url", shuffledCards[index]); //image url to its dataset and add class "unmatched";
+    $(this).addClass("unmatched");
+    $(this).css("backgroundPosition", "center"); // style background position
+    $(this).on("click", func) //add event listener to each image;
+  }) //itereate over all divs with class of "cell" and attach unique
 
 }
 
 ///function that is passed to the "click" event listener attached to each image;
 function func() {
-  this.style.background = 'url(' + this.dataset.icon + ')'; //set each background url to its unique dataset
-  this.style.backgroundPosition = "center"; //set background position for each image
-  this.classList.add("clicked"); //add class clicked if clicked;
-  counter++; //incrememt counter by 1
-  document.getElementById('counter').innerHTML = counter //change display number of counter
+  $(this).css("background-image", 'url(' + $(this).attr("data-url") + ')'); //set each background url to its unique dataset
+  $(this).css("backgroundPosition", "center"); //set background position for each image
+  $(this).addClass("clicked"); //add class clicked if clicked;
+  counter = counter + 1; //incrememt counter by 1
+  $('#counter').html(counter) //change display number of counter
   console.log(counter);
   checkMatch(); //run checkMatch function to see if theres a match
 }
 
 //function checkMatch is called after image is clicked on.
 function checkMatch() {
-  var clickedItems = document.getElementsByClassName("clicked") //get all elements with class of clicked
-  console.log(clickedItems);
-  console.log(clickedItems.length)
-  if (clickedItems.length === 2) { //run this code if there are two items clicked
-    if (clickedItems[0].dataset.icon === clickedItems[1].dataset.icon) { //if the two clicked items have the same url, run this code:
-      Array.from(clickedItems).forEach(function(value) { //use array.from in order to create new array instance from array - like object
-        value.style.background = 'url(' + value.dataset.icon + ')'; //for each item in array, make permanent change to background image;
-        value.style.backgroundPosition = 'center';
-        value.classList.remove("unmatched");//remove the unmatched class from image;
+  var $clickedItems = $(".clicked") //get all elements with class of clicked
+  console.log($clickedItems);
+  console.log($clickedItems.length)
+  if ($clickedItems.length === 2) { //run this code if there are two items clicked
+    if ($(($clickedItems)[0]).attr("data-url") === $(($clickedItems)[1]).attr("data-url")) { //if the two clicked items have the same url, run this code:
+      $clickedItems.each(function() { //use array.from in order to create new array instance from array - like object
+        $(this).css("background-image", 'url(' + $(this).attr("data-url") + ')'); //for each item in array, make permanent change to background image;
+        $(this).css("backgroundPosition", "center");
+        $(this).removeClass("unmatched"); //remove the unmatched class from image;
 
       })
-      Array.from(clickedItems).forEach(function(item) { //remove clicked class from both items.
-        item.classList.remove("clicked");
+      $clickedItems.each(function() { //remove clicked class from both items.
+        $(this).removeClass("clicked");
       })
 
-      checkWinner();//check to see if there is a winner after finding a match;
+      checkWinner(); //check to see if there is a winner after finding a match;
     } else { //run this code if there are no matches after two items are clicked;
       setTimeout(function() { // run a settimeout function after 1 sec;
-        Array.from(clickedItems).forEach(function(card) { //iterate over all items with clicked class;
-          card.style.background = 'url(' + moma + ')'; // set backgrounds to default background;
-          card.classList.remove("clicked"); //remove the clicked class;
-          card.style.backgroundPosition = "center"; // set background position.
+        $clickedItems.each(function() { //iterate over all items with clicked class;
+          $(this).css("background-image", 'url(' + moma + ')'); // set backgrounds to default background;
+          $(this).removeClass("clicked"); //remove the clicked class;
+          $(this).css("backgroundPosition", "center"); // set background position.
         })
       }, 1000)
     }
@@ -76,35 +81,36 @@ function checkMatch() {
 
 //run this code to check for winner after each match is found;
 function checkWinner() {
-  var clearAll = document.getElementsByClassName("clicked"); // get both elements that were clicked;
-  var allUnmatched = document.getElementsByClassName("unmatched"); // grab all other elements on board;
-  if (allUnmatched.length === 0) { // if there are no more elements with class of "unmatched", run this code;
+  var $clearAll = $(".clicked"); // get both elements that were clicked;
+  var $allUnmatched = $(".unmatched"); // grab all other elements on board;
+  if ($allUnmatched.length === 0) { // if there are no more elements with class of "unmatched", run this code;
     var name = prompt("Game Over! Please Enter Your Name.")
     localStorage.setItem(name, counter);
-    scoreBoard(name,counter)//alert the player that they won the game;
+    scoreBoard(name, counter) //alert the player that they won the game;
     counter = 0; //reset the score counter to 0;
-    Array.from(clearAll).forEach(function(value) { //remove the clicked class;
-      value.classList.remove("clicked");
+    $clearAll.each(function() { //remove the clicked class;
+      $(this).removeClass("clicked");
     })
-    for (var i = 0; i < divs.length; i++) {
-      divs[i].removeEventListener("click", func) // remove event listeners from each item;
-      divs[i].style.background = 'url(' + moma + ')'; //set background back to original image;
-    }
+    $divs.each(function() {
+      $divs[i].off() // remove event listeners from each item;
+      $divs[i].css("background-image", 'url(' + moma + ')'); //set background back to original image;
+    })
 
+    newGame();
     startGame(); // automatically start a new game for user;
-    return divs;
+    return $divs;
   }
 }
 
 
-function scoreBoard(name){ // scoreboard function to display score of player
+function scoreBoard(name) { // scoreboard function to display score of player
   console.log(name)
   var playerName = localStorage.getItem(name);
-  var scoreList = document.getElementById("score-page");
-  var newItem = document.createElement("li");
-  newItem.innerHTML = name + " : " + playerName;
-  scoreList.appendChild(newItem);
-  console.log(newItem)
+  var $scoreList = $("#score-page");
+  var $newItem = $("<li></li>");
+  $newItem.html(name + " : " + playerName);
+  $scoreList.append($newItem);
+  console.log($newItem)
 }
 
 
